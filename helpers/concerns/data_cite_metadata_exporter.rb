77c2955fe86c00c6d9a87bb945fbd43ca1abfcd8
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-07-16 20:58:15 +0200

    add data_cite metadata exporter concern

---
+++ helpers/concerns/data_cite_metadata_exporter.rb
@@ -0,0 +1,73 @@
+module Sinatra
+  module Concerns
+    module DataCiteMetadataExporter
+
+      def to_date_cite(sub)
+        hash  = {
+          url: sub.publication.first&.to_s,
+          version: sub.version,
+          description: sub.description,
+          publicationYear: sub.released&.year,
+          publisher: sub.publisher.map{|p| p.name}.join(', '),
+          creators: to_data_cite_creators(sub.hasCreator),
+          identifier: search_doi(sub.identifier) || sub.identifier.first&.to_s,
+          identifierType: search_doi(sub.identifier).nil? ? 'Other' : 'DOI',
+          titles: sub.alternative.map { |x| { title: x, titleType: 'AlternativeTitle' } },
+          resourceTypeGeneral: 'Dataset',
+          resourceType: sub.isOfType.to_s.split('/').last
+        }
+
+        identifier = hash.delete(:identifier)
+        identifier_type = hash.delete(:identifierType)
+        hash[:doi] = identifier if identifier_type.eql?('DOI')
+
+        hash[:types] = {
+          resourceTypeGeneral: hash.delete(:resourceTypeGeneral),
+          resourceType: hash.delete(:resourceType),
+        }
+        hash
+      end
+
+      private
+      def to_data_cite_creators(creators)
+        creators.map do |creator|
+          {
+            nameType: creator.agentType.eql?('person') ? 'Personal' : 'Organizational',
+            givenName: creator.name.split(' ').first,
+            familyName: creator.name.split(' ').drop(1).join(' '),
+            creatorName: creator.name,
+            affiliations: to_data_cite_affiliations(creator.affiliations),
+            nameIdentifiers: to_data_cite_identifiers(creator.identifiers)
+          }
+        end
+      end
+
+      def to_data_cite_identifiers(identifiers)
+        identifiers.map do |identifier|
+          {
+            nameIdentifierScheme: identifier.schemaAgency,
+            schemeURI: identifier.schemeURI,
+            nameIdentifier: identifier.notation
+          }
+        end
+
+      end
+
+      def to_data_cite_affiliations(affiliations)
+        affiliations.map do |affiliation|
+          identifier = affiliation.identifiers.first
+          {
+            affiliationIdentifierScheme: identifier&.schemaAgency,
+            affiliationIdentifier: "#{identifier&.schemeURI}#{identifier&.notation}",
+            affiliation: affiliation.name
+          }
+        end
+      end
+
+      def search_doi(identifiers)
+        identifiers.select { |i| i.start_with?('http://doi.org') || i.start_with?('https://doi.org') }.first&.to_s
+      end
+
+    end
+  end
+end
