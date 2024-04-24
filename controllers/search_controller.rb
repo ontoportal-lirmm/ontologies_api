0fada75c86b2c9b5f229d893d987dbfc5feab01f
Syphax bouazzouni <gs_bouazzouni@esi.dz>
2024-04-24 02:55:48 +0200

    Feature: Add accessibility security to ontology metadata & content search results  (#74)
    * add ontology accessibility restriction  to ontology metadata search
    
    * add ontology accessibility restriction  to ontology content search
    
    * add search results accessibility security test

--- controllers/search_controller.rb
+++ controllers/search_controller.rb
@@ -22,7 +22,7 @@ class SearchController < ApplicationController
         format = params.fetch("hasOntologyLanguage", "").split(',')
         is_of_type = params.fetch("isOfType", "").split(',')
         has_format = params.fetch("hasFormat", "").split(',')
-        visibility = params["visibility"]&.presence || "public"
+        visibility = params["visibility"]
         show_views = params["show_views"] == 'true'
         sort = params.fetch("sort", "score desc, ontology_name_sort asc, ontology_acronym_sort asc")
         page, page_size = page_params
@@ -30,12 +30,12 @@ class SearchController < ApplicationController
         fq = [
           'resource_model:"ontology_submission"',
           'submissionStatus_txt:ERROR_* OR submissionStatus_txt:"RDF" OR submissionStatus_txt:"UPLOADED"',
-          "ontology_viewingRestriction_t:#{visibility}",
           groups.map { |x| "ontology_group_txt:\"http://data.bioontology.org/groups/#{x.upcase}\"" }.join(' OR '),
           categories.map { |x| "ontology_hasDomain_txt:\"http://data.bioontology.org/categories/#{x.upcase}\"" }.join(' OR '),
           languages.map { |x| "naturalLanguage_txt:\"#{x.downcase}\"" }.join(' OR '),
         ]
 
+        fq << "ontology_viewingRestriction_t:#{visibility}" unless visibility.blank?
         fq << "!ontology_viewOf_t:*" unless show_views
 
         fq << format.map { |x| "hasOntologyLanguage_t:\"http://data.bioontology.org/ontology_formats/#{x}\"" }.join(' OR ') unless format.blank?
@@ -75,7 +75,15 @@ class SearchController < ApplicationController
           old_resource_id = acronyms_ids[acronym]
           old_id = old_resource_id.split('/').last.to_i rescue 0
 
-          if acronym.blank? || old_id && id && (id <= old_id)
+          already_found = (old_id && id && (id <= old_id))
+          not_restricted = (doc["ontology_viewingRestriction_t"]&.eql?('public') || current_user&.admin?)
+          user_not_restricted = not_restricted ||
+            Array(doc["ontology_viewingRestriction_txt"]).any? {|u| u.split(' ').last == current_user&.username} ||
+            Array(doc["ontology_acl_txt"]).any? {|u| u.split(' ').last == current_user&.username}
+
+          user_restricted = !user_not_restricted
+
+          if acronym.blank? || already_found || user_restricted
             total_found -= 1
             next
           end
@@ -99,9 +107,25 @@ class SearchController < ApplicationController
       get '/content' do
         query = params[:query] || params[:q]
         page, page_size = page_params
+
         ontologies = params.fetch("ontologies", "").split(',')
+
+        unless current_user&.admin?
+          restricted_acronyms = restricted_ontologies_to_acronyms(params)
+          ontologies = ontologies.empty? ? restricted_acronyms : ontologies & restricted_acronyms
+        end
+
+
         types = params.fetch("types", "").split(',')
         qf = params.fetch("qf", "")
+
+        qf = [
+          "ontology_t^100 resource_id^10",
+          "http___www.w3.org_2004_02_skos_core_prefLabel_txt^30",
+          "http___www.w3.org_2004_02_skos_core_prefLabel_t^30",
+          "http___www.w3.org_2000_01_rdf-schema_label_txt^30",
+          "http___www.w3.org_2000_01_rdf-schema_label_t^30",
+        ].join(' ') if qf.blank?
 
         fq = []
 
@@ -117,7 +141,7 @@ class SearchController < ApplicationController
         docs = resp["response"]["docs"]
 
 
-        reply 200,page_object(docs, total_found)
+        reply 200, page_object(docs, total_found)
       end
     end
 
