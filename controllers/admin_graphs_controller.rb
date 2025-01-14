05115d7075aab432c9fc699da4ac58df57cd6905
Syphax <gs_bouazzouni@esi.dz>
2025-01-14 14:08:31 +0100

    extract graph count path in a constant

--- controllers/admin_graphs_controller.rb
+++ controllers/admin_graphs_controller.rb
@@ -2,6 +2,7 @@ require 'ncbo_cron/graphs_counts'
 class AdminGraphsController < ApplicationController
 
   namespace '/admin' do
+    GRAPH_COUNT_REPORT_PATH = NcboCron.settings.graphs_counts_report_path
     before do
       if LinkedData.settings.enable_security && (!env['REMOTE_USER'] || !env['REMOTE_USER'].admin?)
         error 403, 'Access denied'
@@ -9,14 +10,12 @@ class AdminGraphsController < ApplicationController
     end
 
     get '/graphs' do
-      file_path = NcboCron.settings.graph_counts_report_path
-      output = NcboCron::GraphsCounts.new.read_graph_counts(file_path)
+      output = NcboCron::GraphsCounts.new(nil, GRAPH_COUNT_REPORT_PATH).read_graph_counts
       reply output
     end
 
     post '/graphs' do
-      file_path = NcboCron.settings.graph_counts_report_path
-      NcboCron::GraphsCounts.new.run(Logger.new($stdout), file_path)
+      NcboCron::GraphsCounts.new(nil, GRAPH_COUNT_REPORT_PATH).run
       reply({ message: 'Graph counts generated', status: 200 })
     end
 
