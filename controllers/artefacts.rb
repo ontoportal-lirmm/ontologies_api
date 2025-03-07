354023f0fee97ebb3ee09e383a93df05344ed6e8
Imad Bourouche <ji_bourouche@esi.dz>
2025-03-07 01:46:06 +0100

    regroupe all routes in resources route

--- controllers/artefacts.rb
+++ controllers/artefacts.rb
@@ -59,61 +59,106 @@ class ArtefactsController < ApplicationController
 
         # Ressources
         namespace "/:artefactID/resources" do
+            get do
+                ontology, submission = get_latest_submission
+                _, page, size = settings_params(LinkedData::Models::Class).first(3)
+                size_per_route = (size / 5).to_i
+                
+                resource_types = [
+                  LinkedData::Models::Class,
+                  LinkedData::Models::Instance,
+                  LinkedData::Models::SKOS::Scheme,
+                  LinkedData::Models::SKOS::Collection,
+                  LinkedData::Models::SKOS::Label
+                ]
+
+                resources = resource_types.flat_map do |model|
+                  handle_resource_request(model, page, size_per_route, model.goo_attrs_to_load([])).to_a
+                end
+
+                reply Goo::Base::Page.new(page, size, resources.length, resources)
+            end
+          
             get '/classes' do
-                paginated = params["page"] || params["pagesize"]
-                reply handle_resource_request(LinkedData::Models::Class, paging: paginated.present?)
+                ontology, submission = get_latest_submission
+                type = LinkedData::Models::Class.class_rdf_type(submission)
+                attributes, page, size = settings_params(LinkedData::Models::Class).first(3)
+                
+                if type == RDF::OWL[:Class]
+                    attributes.reject! { |x| x.is_a?(Hash) }
+                    reply handle_resource_request(LinkedData::Models::Class, page, size, attributes)
+                else
+                    reply empty_page(page, size)
+                end
             end
-
+          
             get '/concepts' do
-                redirect "/ontologies/#{params["artefactID"]}/classes", 301
+                ontology, submission = get_latest_submission
+                type = LinkedData::Models::Class.class_rdf_type(submission)
+                attributes, page, size = settings_params(LinkedData::Models::Class).first(3)
+                
+                if type.to_s == "http://www.w3.org/2004/02/skos/core#Concept"
+                    attributes.reject! { |x| x.is_a?(Hash) }
+                    reply handle_resource_request(LinkedData::Models::Class, page, size, attributes)
+                else
+                    reply empty_page(page, size)
+                end
             end
-
+          
             get '/properties' do
-                redirect "/ontologies/#{params["artefactID"]}/properties", 301
+                ont, latest_submission = get_latest_submission
+                begin
+                    props = ont.properties(latest_submission)
+                rescue LinkedData::Models::Ontology::ParsedSubmissionError => e
+                    error 404, e.message
+                end
+                
+                _, page, size = settings_params(LinkedData::Models::OntologyProperty).first(3)
+                reply Goo::Base::Page.new(page, size, props.length, props)
             end
-
-            get '/individuals' do
-                paginated = params["page"] || params["pagesize"]
-                reply handle_resource_request(LinkedData::Models::Instance, paging: paginated.present?)
+          
+            %w[individuals schemes collections labels].each do |resource_type|
+                get "/#{resource_type}" do
+                    model_class = case resource_type
+                        when 'individuals' then LinkedData::Models::Instance
+                        when 'schemes' then LinkedData::Models::SKOS::Scheme
+                        when 'collections' then LinkedData::Models::SKOS::Collection
+                        when 'labels' then LinkedData::Models::SKOS::Label
+                    end
+                    
+                    attributes, page, size = settings_params(model_class).first(3)
+                    attributes.reject! { |x| x.is_a?(Hash) }
+                    reply handle_resource_request(model_class, page, size, attributes)
+                end
             end
-
-            get '/schemes' do
-                paginated = params["page"] || params["pagesize"]
-                reply handle_resource_request(LinkedData::Models::SKOS::Scheme, paging: paginated.present?)
-            end
-
-            get '/collections' do
-                paginated = params["page"] || params["pagesize"]
-                reply handle_resource_request(LinkedData::Models::SKOS::Collection, paging: paginated.present?, clean_attributes: true)
-            end
-        
-            get '/labels' do
-                paginated = params["page"] || params["pagesize"]
-                reply handle_resource_request(LinkedData::Models::SKOS::Label, paging: paginated.present?, clean_attributes: true)
-            end
-
+          
             private
             
-            def handle_resource_request(model, paging: false, clean_attributes: false)
-                artefact, distribution = get_artefact_and_distribution
-                check_last_modified_segment(model, [@params["artefactID"]])
-                attributes, page, size = settings_params(model).first(3)
-                attributes.delete_if { |x| x.is_a?(Hash) } if clean_attributes
-                index = model.where.in(distribution.submission)
-                data = paging ? index.include(attributes).page(page, size).all : index.include(attributes).all
-                return data
+            def empty_page(page, size)
+                Goo::Base::Page.new(page, size, 0, [])
             end
-
-            def get_artefact_and_distribution
-                artefact = LinkedData::Models::SemanticArtefact.find(@params["artefactID"])
-                error 404, "You must provide a valid `artefactID` to retrieve an artefact" if artefact.nil?
-                check_last_modified(artefact)
-                latest_distribution = artefact.latest_distribution(status: [:RDF])
-                error 404, "Artefact #{@params["artefactID"]} distribution not found." if latest_distribution.nil?
-                if !latest_distribution.submission.ready?(status: [:RDF])
-                    error 404, "Artefact #{params["artefactID"]} distribution #{latest_distribution.distributionId} has not been parsed."
+            
+            def handle_resource_request(model, page, size, attributes)
+                _, latest_submission = get_latest_submission
+                check_last_modified_segment(model, [@params["artefactID"]])
+                model.where.in(latest_submission).include(attributes).page(page, size).all
+            end
+          
+            def get_latest_submission
+                @ontology ||= Ontology.find(@params["artefactID"]).first
+                error 404, "You must provide a valid `artefactID` to retrieve an artefact" if @ontology.nil?
+                
+                check_last_modified(@ontology)
+                
+                @latest_submission ||= @ontology.latest_submission(status: [:RDF])
+                error 404, "Artefact #{@params["artefactID"]} distribution not found." if @latest_submission.nil?
+                
+                unless @latest_submission.ready?(status: [:RDF])
+                    error 404, "Artefact #{params["artefactID"]} distribution #{@latest_submission.submissionId} has not been parsed."
                 end
-                return artefact, latest_distribution
+                
+                @latest_submission.bring(*LinkedData::Models::OntologySubmission.goo_attrs_to_load([]))
+                return @ontology, @latest_submission
             end
         end
 
